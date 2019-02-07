c69be100a3885b151f503ecb19be6db32ec2acda
Federico Teotini <templarblack.fed03@gmail.com>
2019-02-07 12:22:50 +0100

    First attempt at rendering nodes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React, { Component } from "react";
 import "./App.css";
 // import jsonn from "./dummydata.json";
-// import { store } from "./Services/Store";
+import { store } from "./Services/Store";
 import { Background } from "./Background";
 import { ArrowHead } from "./ArrowHead";
+import { Group } from "@vx/group";
 
 class App extends Component {
-  // store = store;
+  store = store;
   svgHeight = 1000;
   svgWidth = 1200;
+  constructor(props) {
+    super(props);
+    this.state = {
+      nodes: this.store.nodes
+    };
+  }
+
+  renderNodes() {
+    const { nodes } = this.state;
+    return nodes.map(node => {});
+  }
 
   render() {
     return (
@@ -18,6 +30,7 @@ class App extends Component {
             <ArrowHead markerSize={10} />
           </defs>
           <Background width={this.svgWidth} height={this.svgHeight} />
+          <Group>{this.renderNodes()}</Group>
         </svg>
       </div>
     );
