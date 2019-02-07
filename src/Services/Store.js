64a80b1fcceb9a0709b79276b7742d3a4526f7b0
Federico Teotini <templarblack.fed03@gmail.com>
2019-02-07 12:21:03 +0100

    First concept of global store

---
+++ src/Services/Store.js
@@ -0,0 +1,30 @@
+// @ts-nocheck
+import { NodeData } from "../Node/Models/NodeData";
+import { Node } from "../Node/Models/Node";
+import { EdgeData } from "../Edge/Models/EdgeData";
+import uuid from "uuid/v1";
+
+class Store {
+  nodes = [];
+  edges = [];
+
+  loadPayload(jsonObj) {
+    jsonObj.nodes.forEach(({ id, label, isInitial, isFinal, metadata }) => {
+      this.nodes.push(new NodeData(id, label, isInitial, isFinal, metadata));
+    });
+
+    jsonObj.edges.forEach(({ srcNodeId, trgNodeId, metadata }) => {
+      this.edges.push(new EdgeData(srcNodeId, trgNodeId, metadata));
+    });
+  }
+
+  createNode([x, y]) {
+    const data = new NodeData(uuid(), "", false, false);
+    this.nodes.push(new Node(data, x, y));
+  }
+}
+
+const store = new Store();
+Object.freeze(store);
+
+export { store };
